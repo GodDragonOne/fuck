3cda483a59833c168849f8e73089359f5f502315
DESKTOP-P2TFNKL\User <stu9809264@gamil.com>
2022-12-22 12:34:22 +0800

    test

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/test', function () {
+    $num = 100;
+    $str1 = "$num is 100";
+    $str2 = '$num is 100';
+    each($str1);
+    each($str2);
+});
+
+//示範怎麼pus
